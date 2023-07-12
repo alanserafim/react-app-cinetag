b374ef4b932fdd877db37bbcca7d3b9fcb9eeb64
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-07-12 11:02:02 -0300

    aula 04: rotas

--- README.md
+++ README.md
@@ -12,4 +12,4 @@
 | 1 | Inicio, Cabecalho, CabecalhoLink, Rodape |  Estruturando o projeto, importando fontes, imagens, componentização, css module.
 | 2 | Banner, Card, Container, Titulo |  Componentização, rotas, props e children, importando a partir de json, array.map
 | 3 | FavoritoContext |  Criar contextos através da ContextAPI, Construir hooks personalizados, procurando valores com array.some, excluindo valores com array.filter, React Context DevTool.
-
+| 3 | PaginaBase, NaoEncontrada |  Criar rotas dinâmicas, enviar parâmetros via URL, receber parâmetros com o hook useParams, planejar rotas para URLs não existentes, construir rotas aninhadas.
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,26 +2,29 @@ import { useFavoritoContext } from 'context/Favoritos'
 import styles from './Card.module.css'
 import iconeFavoritar from './favoritar.png'
 import iconeDesfavoritar from './desfavoritar.png'
+import { Link } from 'react-router-dom';
 
-export default function Card({ id, titulo, capa}) {
+export default function Card({ id, titulo, capa }) {
   const { favorito, adicionarFavorito } = useFavoritoContext();
   const ehFavorito = favorito.some((fav) => fav.id === id)
   const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar
 
   return (
     <div className={styles.container}>
-    <img 
-        src={capa}
-        alt={titulo}
-        className={styles.capa}
-    />
-    <h2>{titulo}</h2>
-    <img
+      <Link className={styles.link} to={`/${id}`}>
+        <img
+          src={capa}
+          alt={titulo}
+          className={styles.capa}
+        />
+      <h2>{titulo}</h2>
+      </Link>
+      <img
         src={icone}
         alt='Favoritar filme'
         className={styles.favoritar}
-        onClick={() => {adicionarFavorito({id, titulo, capa})}}
-    />
+        onClick={() => { adicionarFavorito({ id, titulo, capa }) }}
+      />
     </div>
   )
 }
